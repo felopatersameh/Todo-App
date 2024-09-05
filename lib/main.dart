498be45c4b4e0bf7d83957ee62aaa0518d98a3df
Felopater Sameh <felopaters37@gmail.com>
2024-09-05 17:52:51 +0300

    reactor Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'home_screen.dart';
+import 'main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Todo',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: MainScreen(),
     );
   }
 }
